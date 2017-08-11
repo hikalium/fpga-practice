e7ee7a62c46945dadf019bf245b99a698dbf76bb
hikalium <hikalium@hikalium.com>
2017-08-12 04:51:25 +0900

    Impl logic ram sample

--- 005_regfile_test/top.v
+++ 005_regfile_test/top.v
@@ -12,16 +12,17 @@ output [3:0] segsel;
 reg [3:0] led;
 assign nled = ~led;
 
-wire [3:0] btn;
 wire [3:0] btn_down;
 
 reg [7:0] addr = 8'h00;
 
-//Beep bp(clk, 32'd56818, ~btn[0], beep);
-LED7Seg led7(clk, seg, segsel, {8'h00, addr});
 
-ButtonLatch btnlatch(clk, ~nbtn, btn_down);
-defparam btnlatch.PortWidth = 4;
+wire [7:0] memdata = mem[addr];
+reg [7:0] mem [255:0];
+
+initial begin
+	$readmemh("ram.hex", mem);
+end
 
 always @(posedge clk) begin
 	case(btn_down[1:0])
@@ -29,6 +30,15 @@ always @(posedge clk) begin
 		2'b10: addr = addr - 1'd1;
 		default: addr = addr;
 	endcase
+	case(btn_down[3:2])
+		2'b01: mem[addr] = mem[addr] + 1'd1;
+		2'b10: mem[addr] = mem[addr] - 1'd1;
+	endcase
 end
+
+//Beep bp(clk, 32'd56818, ~btn[0], beep);
+LED7Seg led7(clk, seg, segsel, {addr, memdata});
+ButtonLatch btnlatch(clk, ~nbtn, btn_down);
+defparam btnlatch.PortWidth = 4;
 
 endmodule
